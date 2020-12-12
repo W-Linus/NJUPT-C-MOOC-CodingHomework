7824f812e442a820b35f2a5e63959ce504f66e9d
W-Linus <75426289+W-Linus@users.noreply.github.com>
2020-12-12 23:45:29 +0800

    Add files via upload

---
+++ Section 5 函数的基本知识/1.volandsur.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+double r, h, surtotal, voltotal;
+double sur(double r, double h);
+double vol(double r, double h);
+
+int main() {
+	scanf("%lf%lf", &r, &h);
+	sur(r, h);
+	vol(r, h);
+	printf("radius=%lf, height=%lf, Area of surface=%lf, Volume=%lf\n", r, h, surtotal, voltotal);
+	return 0;
+}
+
+double sur(double r, double h) {
+	double c, s, l;
+	l = sqrt((r * r) + (h * h));
+	c = r * r * 3.14;
+	s = ((2 * r * 3.14) * l) / 2;
+	surtotal = c + s;
+	return surtotal;
+}
+
+double vol(double r, double h) {
+	voltotal = ((r * r * 3.14) * h) / 3;
+	return voltotal;
+}
---
+++ Section 5 函数的基本知识/2.Goldbach.cpp
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+
+int a, b, c, flagb, flagc, ctrl = 1;
+void judgeprime(int b, int c);
+
+
+int main() {
+	for (a = 4; a <= 2000; a++) {
+		flagc = 1, flagb = 1;
+		if (a % 2 == 0) {
+			for (b = 2; b <= 2000; b++) {
+				c = a - b;
+				judgeprime(b, c);
+				if (flagb == 1 && flagc == 1) {
+					if (ctrl < 4) {
+						printf("%4d=%4d+%4d ", a, b, c);
+					}
+					if (ctrl == 4) {
+						printf("%4d=%4d+%4d\n", a, b, c);
+						ctrl = 0;
+					}
+					ctrl++;
+					break;
+				}
+			}
+		}
+	}
+
+}
+
+void judgeprime(int b, int c) {
+	for (int i = 2; i < b; i++) {
+		if (b % i == 0) {
+			flagb = 0;
+			break;
+		}
+		else {
+			flagb = 1;
+		}
+	}
+	for (int j = 2; j < c; j++) {
+		if (c % j == 0) {
+			flagc = 0;
+			break;
+		}
+		else {
+			flagc = 1;
+		}
+	}
+
+}
