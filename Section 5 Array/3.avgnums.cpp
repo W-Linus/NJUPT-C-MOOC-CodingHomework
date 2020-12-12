ff4952fda89c1a87a6a9568d8c4538dee31722cf
W-Linus <75426289+W-Linus@users.noreply.github.com>
2020-12-12 22:28:47 +0800

    Add files via upload
    南京邮电大学高级语言程序设计第五章慕课编程作业

---
+++ Section 5 Array/3.avgnums.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+int a[10],tool,min,max,minr,maxr;
+double div=0;
+
+int main() {
+	for (int i = 0; i < 10; i++) {
+		scanf_s("%d", &a[i]);
+	}
+	for (int j = 0; j < 9; j++) {
+		if (a[j] > a[j + 1]) {
+			tool = a[j];
+			a[j] = a[j + 1];
+			a[j + 1] = tool;
+			j = 0;
+		}
+	}
+	/*if (a[0] == a[9]) {
+		printf("%lf\n",(double)a[0]);
+		return 0;
+	}*/
+	min = a[0];
+	max = a[9];
+	for (int k = 0; k < 10; k++) {
+		if (a[k] == min) {
+			minr++;
+		}
+		if (a[k] == max) {
+			maxr++;
+		}
+	}
+		
+	for(int l = minr; l < 10 - maxr; l++) {
+		div = div + a[l];
+	}
+	div =div/(double)(10-(minr+maxr));
+	printf("%lf\n", div);
+	return 0;
+}
