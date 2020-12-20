5924f342711b23196311f8c762d0037ad7bccaa7
W-Linus <75426289+W-Linus@users.noreply.github.com>
2020-12-20 15:11:39 +0800

    Add files via upload

---
+++ Section 7 指针/changeSort.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+void sort(int* a, int n);
+int a[10] = {}, n;
+
+int main() {
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &a[i]);
+	}
+	sort(a, n);
+	for (int i = 0; i < n; i++) {
+		if (i != n - 1) {
+			printf("%d  ", a[i]);
+		}
+		else {
+			printf("%d  ", a[i]);
+			printf("\n");
+		}
+	}
+	return 0;
+}
+
+void sort(int* a, int n) {
+	int temp;
+	for (int i = 0; i < n-1; i++) {
+		if (a[i] > a[i + 1]) {
+			temp = a[i + 1];
+			a[i + 1] = a[i];
+			a[i] = temp;
+			i = 0;
+		}
+	}
+}
