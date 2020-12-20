7ab0218ce2cfcea28cb5a4fb2c01cabf5a8eef57
W-Linus <75426289+W-Linus@users.noreply.github.com>
2020-12-20 15:13:40 +0800

    Update sumanddiff.cpp

--- Section 7 指针/sumanddiff.cpp
+++ Section 7 指针/sumanddiff.cpp
@@ -5,7 +5,7 @@ int SumDiff(int a, int b, int* dif);
 int main()
 {
     int a, b, sum, dif;
-    scanf_s("%d%d", &a, &b);
+    scanf("%d%d", &a, &b);
     sum = SumDiff(a, b, &dif);
     printf("sum=%d, difference=%d\n", sum, dif);
     return 0;
